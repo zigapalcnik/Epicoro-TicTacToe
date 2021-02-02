fa3084e24f465a1c2a58627f26d1644582313dc5
Žiga <ziga.palcnik1@gmail.com>
2021-02-02 21:18:24 +0100

    feat: created create user component
    showing this component on home page

--- tic-tac-toe/src/app/@pages/home/home.component.ts
+++ tic-tac-toe/src/app/@pages/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NbDialogService } from '@nebular/theme';
+import { CreateNewUserComponent } from '../../@theme/components/create-new-user/create-new-user.component';
+import { take } from 'rxjs/operators';
+import { LoggerFactory } from '../../@core/log/logger-factory';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  private static logger = LoggerFactory.getLogger(HomeComponent.name);
 
-  constructor() { }
+  username: string;
+  constructor(private readonly dialogService: NbDialogService) { }
 
   ngOnInit(): void {
+    this.createUserDialog();
+  }
+
+  createUserDialog(): void {
+    this.dialogService.open(CreateNewUserComponent, {
+    }).onClose
+      .pipe(take(1))
+      .subscribe((result: string) => {
+        this.username = result;
+        HomeComponent.logger.info(this.username, 'Username test');
+      });
   }
 
 }
