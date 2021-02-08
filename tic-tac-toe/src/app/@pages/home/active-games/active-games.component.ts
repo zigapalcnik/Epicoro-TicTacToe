5385d2124e5cf1c5b2bff18b95204733644f7383
Žiga <ziga.palcnik1@gmail.com>
2021-02-08 12:29:04 +0100

    feat(active-games): updated active games component.

--- tic-tac-toe/src/app/@pages/home/active-games/active-games.component.ts
+++ tic-tac-toe/src/app/@pages/home/active-games/active-games.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { GameStatus } from '../../../@core/data/game.service';
 import { User } from '../../../@core/data/user.service';
+import { GameState, PlayingSign } from '../../game/game.component';
 
 @Component({
   selector: 'app-active-games',
@@ -10,4 +11,34 @@ import { User } from '../../../@core/data/user.service';
 export class ActiveGamesComponent {
   @Input() games: GameStatus[];
   @Input() user: User;
+
+  getXUserName(game: GameStatus): string {
+    return `${ game.playerX?.username }`;
+  }
+
+  getYUserName(game: GameStatus): string {
+    return `${ game.playerO?.username ?? 'Opponent' }`;
+  }
+
+  getXBadge(game: GameStatus, isText: boolean = true): string {
+    if (game.playerX?.id === this.user.id) {
+      if (game.currentPlayerSign === PlayingSign.X) {
+          return isText ? `Your turn!` : 'p-1 badge badge-warning';
+        } else {
+        return isText ? `${ game.playerO.username } turn`: 'p-1 badge badge-info';
+      }
+    }
+    return '';
+  }
+
+  getOBadge(game: GameStatus, isText: boolean = true): string {
+    if (game.playerO?.id === this.user.id) {
+      if (game.currentPlayerSign === PlayingSign.O) {
+        return isText ? `Your turn!` : 'p-1 badge badge-warning';
+      } else {
+        return isText ? `${ game.playerX.username } turn` : 'p-1 badge badge-info';
+      }
+    }
+    return '';
+  }
 }
